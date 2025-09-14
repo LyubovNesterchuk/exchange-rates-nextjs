58ba7abbcc54fd33ec2b501ad8807ce335c4cd4a
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-09-14 20:33:35 +0300

    o

--- components/Grid/Grid.tsx
+++ components/Grid/Grid.tsx
@@ -1,4 +1,9 @@
+import { ReactNode } from 'react';
 import style from './Grid.module.css';
+
+interface GridProps {
+  children: ReactNode;
+}
 
 export default function Grid({ children }: GridProps) {
   return <ul className={style.list}>{children}</ul>;
